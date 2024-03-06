254abb9a5fab84c6231aad900fec390fbcf2b69f
PauloPZ <pc.zuit@gmail.com>
2024-03-06 10:48:53 -0300

    RotasApp

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+// Pacotes  Stack Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Criando uma instância do STACK NAVIGATOR,
+// Isso nos permite utilizar comandos para definir a navegação da aplicação.
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
+  );
+}
+
+function Home() {
+  return (
+
+<View></View>
   );
 }
 
